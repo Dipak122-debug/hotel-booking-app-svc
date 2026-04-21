fe97bbbe3e1bd0100afc4ac53ec1ae73c2d7a617
Dipak122-debug <dipak628das@gmail.com>
2026-04-21 05:43:18 +0530

    cleaned up application.properties by removing unnecessary whitespace

---
+++ hotel-svc/src/test/java/com/project/hotel_svc/HotelSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hotel_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
